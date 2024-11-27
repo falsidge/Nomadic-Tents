beddc37b28a36fb98b27dce4f1bdde46d153dae0
falsidge <tesetings@gmail.com>
2024-11-27 04:49:45 -0800

    Fix creative tab

--- src/main/java/nomadictents/NTRegistry.java
+++ src/main/java/nomadictents/NTRegistry.java
@@ -3,6 +3,7 @@ package nomadictents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -19,6 +20,10 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 //import net.minecraft.world.level.material.Material;
 //import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -48,9 +53,7 @@ import nomadictents.tileentity.TentDoorBlockEntity;
 import nomadictents.util.TentSize;
 import nomadictents.util.TentType;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Supplier;
 
 public final class NTRegistry {
@@ -65,8 +68,8 @@ public final class NTRegistry {
     private static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, NomadicTents.MODID);
 
 
-    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_MODE_TABS.register("tents", ()->
-                CreativeModeTab.builder().icon(()->new ItemStack(NTRegistry.TINY_YURT.get())).displayItems((displayParameter, output)->
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_MODE_TABS.register("nomadic_tents", ()->
+                CreativeModeTab.builder().icon(()->new ItemStack(NTRegistry.TINY_YURT.get())).title(Component.translatable("itemGroup.nomadictents")).displayItems((displayParameter, output)->
                         {
                             output.accept(NTRegistry.TENT_CANVAS.get());
                             output.accept(NTRegistry.YURT_SECTION.get());
@@ -98,6 +101,7 @@ public final class NTRegistry {
         RECIPE_SERIALIZERS.register(FMLJavaModLoadingContext.get().getModEventBus());
         // event listeners
         FMLJavaModLoadingContext.get().getModEventBus().addListener(NTRegistry::onSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(NTRegistry::buildContents);
         // blocks
         registerBlocks();
         // items
@@ -108,20 +112,20 @@ public final class NTRegistry {
         // register tepee blocks
         for (final TepeeBlock.Type type : TepeeBlock.Type.values()) {
             BLOCKS.register(type.getSerializedName() + "_tepee_wall", () ->
-                    new TepeeBlock(type, BlockBehaviour.Properties.copy(Blocks.BARRIER)
+                    new TepeeBlock(type, BlockBehaviour.Properties.of().mapColor(MapColor.TERRACOTTA_WHITE).pushReaction(PushReaction.BLOCK)
                             .sound(SoundType.WOOL)));
         }
         // register shamiyana blocks
         for (DyeColor color : DyeColor.values()) {
             BLOCKS.register(color.getSerializedName() + "_shamiyana_wall", () ->
-                    new ShamiyanaWallBlock(color, BlockBehaviour.Properties.copy(Blocks.BARRIER)
+                    new ShamiyanaWallBlock(color, BlockBehaviour.Properties.of().mapColor(color.getMapColor()).pushReaction(PushReaction.BLOCK)
                             .sound(SoundType.WOOL)));
         }
         // register door blocks
         for (TentType type : TentType.values()) {
             for (TentSize width : TentSize.values()) {
                 BLOCKS.register(width.getSerializedName() + "_" + type.getSerializedName() + "_door", () ->
-                        new TentDoorBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+                        new TentDoorBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOL).pushReaction(PushReaction.BLOCK)
                                 .sound(SoundType.WOOL)));
             }
         }
@@ -142,12 +146,15 @@ public final class NTRegistry {
     public static final RegistryObject<Item> OBSIDIAN_TENT_SHOVEL = ITEMS.register("obsidian_tent_shovel", () -> new TentShovelItem(new Item.Properties()));
     public static final RegistryObject<Item> DIAMOND_TENT_SHOVEL = ITEMS.register("diamond_tent_shovel", () -> new TentShovelItem(new Item.Properties()));
 
+    public static final List<RegistryObject<Item>> TENTS = new ArrayList<>();
+    public static final List<RegistryObject<BlockItem>> CREATIVE_TAB_BLOCKS = new ArrayList<>();
+
     private static void registerItems() {
         // register tents for each type and size
         for (TentType type : TentType.values()) {
             for (TentSize width : TentSize.values()) {
-                ITEMS.register(width.getSerializedName() + "_" + type.getSerializedName(), () ->
-                        new TentItem(type, width, new Item.Properties().stacksTo(1)));
+                TENTS.add(ITEMS.register(width.getSerializedName() + "_" + type.getSerializedName(), () ->
+                        new TentItem(type, width, new Item.Properties().stacksTo(1))));
             }
         }
 
@@ -176,25 +183,26 @@ public final class NTRegistry {
 
     //// BLOCKS ////
     public static final RegistryObject<Block> YURT_WALL = BLOCKS.register("yurt_wall", () ->
-            new YurtWallBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new YurtWallBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOL).pushReaction(PushReaction.BLOCK)
                     .sound(SoundType.WOOL)));
     public static final RegistryObject<Block> YURT_ROOF = BLOCKS.register("yurt_roof", () ->
-            new YurtRoofBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new YurtRoofBlock(BlockBehaviour.Properties.of().mapColor(DyeColor.LIGHT_BLUE).pushReaction(PushReaction.BLOCK)
                     .sound(SoundType.WOOL)));
     public static final RegistryObject<Block> BEDOUIN_WALL = BLOCKS.register("bedouin_wall", () ->
-            new QuarterTentBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new QuarterTentBlock(BlockBehaviour.Properties.of().mapColor(DyeColor.BROWN).pushReaction(PushReaction.BLOCK)
                     .sound(SoundType.WOOL)));
     public static final RegistryObject<Block> BEDOUIN_ROOF = BLOCKS.register("bedouin_roof", () ->
-            new TentBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new TentBlock(BlockBehaviour.Properties.of().mapColor(DyeColor.BROWN).pushReaction(PushReaction.BLOCK)
                     .sound(SoundType.WOOL)));
     public static final RegistryObject<Block> INDLU_WALL = BLOCKS.register("indlu_wall", () ->
-            new IndluWallBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new IndluWallBlock(BlockBehaviour.Properties.of().mapColor(MapColor.GRASS).pushReaction(PushReaction.BLOCK)
                     .noOcclusion()
                     .isViewBlocking((b, r, p) -> false)
                     .sound(SoundType.GRASS)));
     public static final RegistryObject<Block> RIGID_DIRT = BLOCKS.register("rigid_dirt", () ->
-            new TentBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+            new TentBlock(BlockBehaviour.Properties.of().mapColor(MapColor.DIRT).pushReaction(PushReaction.BLOCK)
                     .sound(SoundType.GRAVEL)));
+
     public static final RegistryObject<Block> DOOR_FRAME = registerFrame("door_frame");
     public static final RegistryObject<Block> YURT_WALL_FRAME = registerFrame("yurt_wall_frame");
     public static final RegistryObject<Block> YURT_ROOF_FRAME = registerFrame("yurt_roof_frame");
@@ -253,7 +261,7 @@ public final class NTRegistry {
 
     private static RegistryObject<Block> registerFrame(final String name) {
         return BLOCKS.register(name, () ->
-                new FrameBlock(BlockBehaviour.Properties.copy(Blocks.BARRIER)
+                new FrameBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).pushReaction(PushReaction.BLOCK)
                         .strength(-1.0F, 3600000.8F)
                         .noCollission().noLootTable().sound(SoundType.WOOD)));
     }
@@ -263,14 +271,30 @@ public final class NTRegistry {
     }
 
     private static RegistryObject<BlockItem> registerItemBlock(final String name, final Supplier<? extends Block> blockSupplier, final boolean group) {
-        return ITEMS.register(name, itemBlock(blockSupplier, group));
+        RegistryObject<BlockItem> newItem =  ITEMS.register(name, itemBlock(blockSupplier, group));
+        if (group)
+        {
+            CREATIVE_TAB_BLOCKS.add(newItem);
+        }
+        return newItem;
     }
 
     private static Supplier<BlockItem> itemBlock(final Supplier<? extends Block> blockSupplier, final boolean group) {
         final Item.Properties props = new Item.Properties();
-//        if (group) {
-//            props.tab(TAB);
-//        }
+
         return () -> new BlockItem(blockSupplier.get(), props);
+    }
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == TAB.getKey()) {
+            for (RegistryObject<Item> tent : TENTS)
+            {
+                event.accept(tent.get());
+            }
+            for (RegistryObject<BlockItem> blocks : CREATIVE_TAB_BLOCKS)
+            {
+                event.accept(blocks.get());
+            }
+        }
     }
 }
